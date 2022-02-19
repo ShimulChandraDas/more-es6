ffda9137918fb6ff358025ba62d4c5eb2d894da7
ShimulChandraDas <msshimul99@gmail.com>
2022-02-19 22:00:23 +0600

    modify

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+const fish = { id: 58, name: "king hilsha", price: 9000, phone: '05412455321223', address: 'chabpur', dress: 'silver' };
+const phone = fish.phone;
+const id = fish.id;
+const name = fish.name;
+const price = fish.price;
+const address = fish.address;
+const dress = fish.dress;
+
+
+console.log(phone);
+console.log(phone, id);
+console.log(phone, id, name);
+console.log(phone, id, name, price);
+console.log(phone, id, name, price, dress);
+console.log(phone, id, name, price, dress, address);
