dba7688ef9cc71ee26a5025a71275a68e52c3dcc
ShimulChandraDas <msshimul99@gmail.com>
2022-02-19 22:22:27 +0600

    32-2 Destructuring Object to extract values to variables

--- destructuring.js
+++ destructuring.js
@@ -1,15 +1,41 @@
 const fish = { id: 58, name: "king hilsha", price: 9000, phone: '05412455321223', address: 'chabpur', dress: 'silver' };
-const phone = fish.phone;
-const id = fish.id;
-const name = fish.name;
-const price = fish.price;
-const address = fish.address;
-const dress = fish.dress;
+// process 1
+// const phone = fish.phone;
+// const id = fish.id;
+// const name = fish.name;
+// const price = fish.price;
+// const address = fish.address;
+// const dress = fish.dress;
+
+//process 2
+const { phone, id, name, price, address, dress } = fish;
+
+/* Or */
+// const { phone, id, name, price, address, dress } = { id: 58, name: "king hilsha", price: 9000, phone: '05412455321223', address: 'chabpur', dress: 'silver' };
 
 
-console.log(phone);
-console.log(phone, id);
-console.log(phone, id, name);
-console.log(phone, id, name, price);
-console.log(phone, id, name, price, dress);
-console.log(phone, id, name, price, dress, address);
+
+// console.log(phone);
+// console.log(phone, id);
+// console.log(phone, id, name);
+// console.log(phone, id, name, price);
+// console.log(phone, id, name, price, dress);
+// console.log(phone, id, name, price, dress, address);
+
+
+const company = {
+    name: 'GP',
+    ceo: { id: 1, name: 'Ajmol', food: 'hajmola' },
+    web: { work: 'web development', employee: 22, framework: 'react' }
+}
+//traditional way 
+// const work = company.web.work;
+// const framework = company.web.framework;
+// const food = company.ceo.food;
+
+//Smart way
+
+const { work, framework } = company.web;
+const { food } = company.ceo;
+
+console.log(work, framework, food);
